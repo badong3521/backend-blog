1f555d74fbe0135831270d7d1569b6e2a603ac8f
BaDong <dongba352001@gmail.com>
2023-12-26 17:28:12 +0700

    fix command packagejson

--- dist/app-apis/requests/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Platform = void 0;
-var Platform;
-(function (Platform) {
-    Platform[Platform["MOBILE"] = 0] = "MOBILE";
-    Platform[Platform["WEB"] = 1] = "WEB";
-})(Platform = exports.Platform || (exports.Platform = {}));
--- dist/app-middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthJwt = exports.VerifySignUp = exports.HttpMiddleware = void 0;
-var http_middleware_1 = require("./http.middleware");
-Object.defineProperty(exports, "HttpMiddleware", { enumerable: true, get: function () { return http_middleware_1.HttpMiddleware; } });
-var verify_signup_1 = require("./verify.signup");
-Object.defineProperty(exports, "VerifySignUp", { enumerable: true, get: function () { return verify_signup_1.VerifySignUp; } });
-var authJwt_1 = require("./authJwt");
-Object.defineProperty(exports, "AuthJwt", { enumerable: true, get: function () { return authJwt_1.AuthJwt; } });
