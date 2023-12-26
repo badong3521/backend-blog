ddd4ab6b62ee25f62fb1fac9ee987a1633d9c3c1
BaDong <dongba352001@gmail.com>
2023-12-26 17:01:24 +0700

    deploy

--- build/configRoute/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function appRoutes(app) {
-    app.use("/sayHi", (req, res) => {
-        res.send("Say Hi");
-    });
-}
-exports.default = appRoutes;
-//# sourceMappingURL=index.js.map
